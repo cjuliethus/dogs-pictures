e3965cf72972f0598343c6f11c03a6993c134609
Julieth urquijo <juliethurquijo@habi.co>
2022-03-25 14:36:56 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import Axios from "axios";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    getDogs();
+  }, []);
+
+  const getDogs = async () => {
+    await Axios.get("https://dog.ceo/api/breeds/image/random/10").then(
+      ({ data }) => {
+        setData(data);
+      }
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-doggos">
+        <div className="section-photos-dogs">
+          {data?.message?.map((item) => {
+            return (
+              <div
+                className="item-img"
+                style={{
+                  background: `url(${item}) no-repeat`,
+                  backgroundSize: "100% 100%",
+                }}
+              >
+                {/* <img src={item} /> */}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
